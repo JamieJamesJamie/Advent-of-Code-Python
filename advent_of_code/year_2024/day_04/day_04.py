c3144c0ea117bac06285fb15523f93a32ba6fb68
Jamie Harris <jamiedodgerification12345@gmail.com>
2024-12-07 00:01:43 +0000

    feat(2024): add day 4 solutions

--- advent_of_code/year_2024/day_04/day_04.py
+++ advent_of_code/year_2024/day_04/day_04.py
@@ -32,28 +32,46 @@ class Day4(Solver):
 
     _X_MIDDLE_MS = _X_MAS_MAPPING["M"] + _X_MAS_MAPPING["S"]
 
-    @staticmethod
-    def parse(puzzle_input: str) -> np.ndarray:
-        return np.array(tuple(tuple(line) for line in puzzle_input.splitlines()))
+    _CORRELATION_MODE = "constant"
 
     @staticmethod
-    def _num_occurrences_diagonal(diagonal_array: np.ndarray) -> int:
-        return np.sum(
-            np.all(diagonal_array.diagonal(axis1=1, axis2=2) == Day4._XMAS_ARRAY, 1)
-        )
+    def _flip_functions(axis: int):
+        return np.asarray, partial(np.flip, axis=axis)
 
     @staticmethod
-    def _num_occurrences_horizontal(horizontal_array: np.ndarray) -> int:
+    def _num_occurrences(horizontal_array: np.ndarray) -> int:
         return np.sum(np.all(horizontal_array == Day4._XMAS_ARRAY, 1))
 
     @staticmethod
-    def _diagonal_sum(wordsearch: np.ndarray) -> int:
-        windows = sliding_window_view(wordsearch, (4, 4))
+    def _non_diagonal_sum(
+        word_search: np.ndarray, window_shape: tuple[int, int]
+    ) -> int:
+        windows = sliding_window_view(word_search, window_shape)
+        reshaped_windows = windows.reshape((np.multiply(*windows.shape[0:2]), 4))
+
+        return sum(
+            Day4._num_occurrences(flip_function(reshaped_windows))
+            for flip_function in Day4._flip_functions(1)
+        )
+
+    @staticmethod
+    def _horizontal_sum(word_search: np.ndarray) -> int:
+        return Day4._non_diagonal_sum(word_search, (1, 4))
+
+    @staticmethod
+    def _vertical_sum(word_search: np.ndarray) -> int:
+        return Day4._non_diagonal_sum(word_search, (4, 1))
+
+    @staticmethod
+    def _diagonal_sum(word_search: np.ndarray) -> int:
+        windows = sliding_window_view(word_search, (4, 4))
         reshaped_windows = windows.reshape((np.multiply(*windows.shape[0:2]), 4, 4))
 
         return sum(
-            Day4._num_occurrences_diagonal(
-                flip_function_1(flip_function_2(reshaped_windows))
+            Day4._num_occurrences(
+                flip_function_1(flip_function_2(reshaped_windows)).diagonal(
+                    axis1=1, axis2=2
+                )
             )
             for flip_function_1, flip_function_2 in product(
                 Day4._flip_functions(1), Day4._flip_functions(2)
@@ -61,51 +79,52 @@ class Day4(Solver):
         )
 
     @staticmethod
-    def _flip_functions(axis: int):
-        return np.asarray, partial(np.flip, axis=axis)
+    def parse(puzzle_input: str) -> np.ndarray:
+        """Parse input for puzzle 4.
 
-    @staticmethod
-    def _horizontal_sum(wordsearch: np.ndarray) -> int:
-        windows = sliding_window_view(wordsearch, (1, 4))
-        reshaped_windows = windows.reshape((np.multiply(*windows.shape[0:2]), 4))
-
-        return sum(
-            Day4._num_occurrences_horizontal(flip_function(reshaped_windows))
-            for flip_function in Day4._flip_functions(1)
-        )
-
-    @staticmethod
-    def _vertical_sum(wordsearch: np.ndarray) -> int:
-        windows = sliding_window_view(wordsearch, (4, 1))
-        reshaped_windows = windows.reshape((np.multiply(*windows.shape[0:2]), 4))
-
-        return sum(
-            Day4._num_occurrences_horizontal(flip_function(reshaped_windows))
-            for flip_function in Day4._flip_functions(1)
-        )
+        :param puzzle_input: Input to parse.
+        :return: The characters in the word search.
+        """
+        return np.array(tuple(tuple(line) for line in puzzle_input.splitlines()))
 
     @staticmethod
     def part1(parsed_input: np.ndarray) -> int:
-        return (
-            Day4._horizontal_sum(parsed_input)
-            + Day4._vertical_sum(parsed_input)
-            + Day4._diagonal_sum(parsed_input)
+        """Solves part 1.
+
+        :param parsed_input: Input to parse.
+        :return: The number of times "XMAS" appears in the word search.
+        """
+        return sum(
+            (
+                Day4._horizontal_sum(parsed_input),
+                Day4._vertical_sum(parsed_input),
+                Day4._diagonal_sum(parsed_input),
+            )
         )
 
     @staticmethod
     def part2(parsed_input: np.ndarray) -> int:
-        letter_map = np.array(
+        """Solves part 2.
+
+        :param parsed_input: Input to parse.
+        :return: The number of "X-MAS" occurrences in the word search.
+        """
+        letter_number_mapping = np.array(
             tuple(
                 tuple(Day4._X_MAS_MAPPING[character] for character in line)
                 for line in parsed_input
             )
         )
 
-        a_locations = letter_map == Day4._X_MAS_MAPPING["A"]
+        a_locations = letter_number_mapping == Day4._X_MAS_MAPPING["A"]
 
         correlated = (
-            correlate(letter_map, Day4._WEIGHTS[0], mode="constant"),
-            correlate(letter_map, Day4._WEIGHTS[1], mode="constant"),
+            correlate(
+                letter_number_mapping, Day4._WEIGHTS[0], mode=Day4._CORRELATION_MODE
+            ),
+            correlate(
+                letter_number_mapping, Day4._WEIGHTS[1], mode=Day4._CORRELATION_MODE
+            ),
         )
 
         cross_middles = (correlated[0] == Day4._X_MIDDLE_MS) & (
--- tests/year_2024/day_04/test_day_04.py
+++ tests/year_2024/day_04/test_day_04.py
@@ -1,9 +1,10 @@
 """Tests for Day4 Solution class."""
 
+import numpy as np
 import pytest
-from year_2024.abstract_test_day_2024 import AbstractTestDay2024
 
 from advent_of_code.year_2024.day_04.day_04 import Day4
+from tests.year_2024.abstract_test_day_2024 import AbstractTestDay2024
 
 
 @pytest.mark.parametrize("example", [1], indirect=True)
@@ -31,7 +32,23 @@ class TestDay(AbstractTestDay2024):
 
         :param example: Parsed input from example file.
         """
-        assert example == 1
+        assert (
+            example
+            == np.array(
+                (
+                    ("M", "M", "M", "S", "X", "X", "M", "A", "S", "M"),
+                    ("M", "S", "A", "M", "X", "M", "S", "M", "S", "A"),
+                    ("A", "M", "X", "S", "X", "M", "A", "A", "M", "M"),
+                    ("M", "S", "A", "M", "A", "S", "M", "S", "M", "X"),
+                    ("X", "M", "A", "S", "A", "M", "X", "A", "M", "M"),
+                    ("X", "X", "A", "M", "M", "X", "X", "A", "M", "A"),
+                    ("S", "M", "S", "M", "S", "A", "S", "X", "S", "S"),
+                    ("S", "A", "X", "A", "M", "A", "S", "A", "A", "A"),
+                    ("M", "A", "M", "M", "M", "X", "M", "M", "M", "M"),
+                    ("M", "X", "M", "X", "A", "X", "M", "A", "S", "X"),
+                )
+            )
+        ).all()
 
     def test_part1_example(self, example):
         """Test part 1 on example input.
