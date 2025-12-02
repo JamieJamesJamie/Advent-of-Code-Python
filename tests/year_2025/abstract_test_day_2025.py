2d1c855fc7ae572a5d7658f8d36947ed7aff6fcf
Jamie Harris <jamiedodgerification12345@gmail.com>
2025-12-02 01:01:40 +0000

    feat(year2025): solve day 1 (#608)

---
+++ tests/year_2025/abstract_test_day_2025.py
@@ -0,0 +1,14 @@
+"""Abstract test class to be used for testing Solution classes for the year
+2025."""
+
+from abc import ABCMeta
+
+from tests.helper.abstract_test_day import AbstractTestDay
+
+
+class AbstractTestDay2025(AbstractTestDay, metaclass=ABCMeta):
+    """Abstract class to be used for testing solutions for the year 2025."""
+
+    @staticmethod
+    def year() -> int:
+        return 2025
